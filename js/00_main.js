3d8613da13697c4ed5b01bd9a9f27b22bbd98486
Buchi-18 <3690@gmail.com>
2023-05-03 16:42:05 +0900

    0503! git add . git st

--- js/00_main.js
+++ js/00_main.js
@@ -1,89 +1,76 @@
-const lines = ["7", "WWWWW"];
+const lines = [
+  "8 9",
+  "0 1 3 4 22 34 60 63 85",
+  "0 35 44 50 53 56 61 90 99",
+  "0 7 15 35 42 51 53 96 100",
+  "0 23 40 41 44 64 67 83 91",
+  "0 5 36 47 59 70 81 87 97",
+  "0 5 11 14 30 34 64 76 90",
+  "0 4 5 22 57 62 90 91 98",
+  "0 8 13 25 35 41 60 68 82",
+  "10",
+  "2 6", //0   0-56  56
+  "3 4", //1  51-35  16   72
+  "6 1", //2  14- 0  14   86
+  "4 4", //3   0-41  41  127
+  "5 4", //4  47-47 ---  ---
+  "8 6", //5  25-41  16  143
+  "1 8", //6  34-63  29  172
+  "5 6", //7  87-70  17  189
+  "4 6", //8  64-64 ---  ---
+  "5 1", //9  70- 0  70  259
+];
 
-function checkBullet(data) {
-  const initBullets = data[1];
-  const bulletsArr = initBullets.split("");
-  const len = bulletsArr.length;
+function fareCalculation(data) {
+  const fareCount = parseInt(data[0].split(" ")[0]);
+  const fareMatrix = data
+    .slice(1, fareCount + 1)
+    .map((fare) => fare.split(" ").map((num) => parseInt(num)));
+  const movementList = data
+    .slice(1 + fareCount + 1)
+    .map((point) => point.split(" ").map((num) => parseInt(num)));
 
-  for (let i = 0; i < len; i++) {
-    const last = bulletsArr.pop();
-    bulletsArr.unshift(last);
-    let allMatch = true;
+  let totalAmount = 0;
+  let nextStartAmount = 0;
 
-    for (let j = 0; j < len; j++) {
-      if (bulletsArr[j] !== initBullets[j]) {
-        allMatch = false;
-        break;
-      }
-    }
-    if (allMatch) {
-      return i + 1;
-    }
+  for (let i = 0; i < movementList.length; i++) {
+    let from = movementList[i][0] - 1;
+    let to = movementList[i][1] - 1;
+    let nextFrom =
+      i < movementList.length - 1 ? movementList[i + 1][0] - 1 : NaN;
+
+    let amount = fareMatrix[from][to];
+    totalAmount += Math.abs(nextStartAmount - amount);
+
+    nextStartAmount = !isNaN(nextFrom) ? fareMatrix[nextFrom][to] : NaN;
   }
 
-  return len;
+  return totalAmount;
 }
 
-console.log(checkBullet(lines));
+console.log(fareCalculation(lines));
+
 
 /* ***** ***** ***** ***** ***** ***** ***** ***** 
 
-const a = [ 'B', 'W', 'W', 'W', 'B', 'W', 'B' ];
-const b = [ 'B', 'W', 'W', 'W', 'B', 'W', 'B' ];
-上記の配列の中身順番も含め完全一致するか判定
-
-items = lines.slice(1);
-const square = items.map((item) => item.split(""));
-
-function checkCrossCount(square) {
-  let crossCount = 0;
-
-  for (let i = 1; i < square.length - 1; i++) {
-    const targetArray = square[i];
-    const aboveArray = square[i - 1];
-    const belowArray = square[i + 1];
-
-    for (let j = 1; j < targetArray.length - 1; j++) {
-      const target = targetArray[j];
-      const leftTarget = targetArray[j - 1];
-      const rightTarget = targetArray[j + 1];
-      const aboveTarget = aboveArray[j];
-      const belowTarget = belowArray[j];
-
-      if (target === "#" && 
-          leftTarget === "#" && 
-          rightTarget === "#" && 
-          aboveTarget === "#" && 
-          belowTarget === "#") {
-        crossCount++;
-      }
-    }
-  }
-  return crossCount;
-}
-
-console.log(checkCrossCount(square));
-
-function checkBullet(data) {
-  const initBullets = data[1];
-  const bulletsArr = initBullets.split("");
-  let shootCount = 0;
-  const len = bulletsArr.length;
-
-  for (let i = 0; i < len; i++) {
-    let allMatch = true;
-    for (let j = 0; j < len; j++) {
-      if (bulletsArr[j] !== initBullets[(i + j) % len]) {
-        allMatch = false;
-        break;
-      }
-    }
-    if (allMatch) {
-      return i + 1;
-    }
-  }
-  return len;
-}
-一回ずつ配列の中身を変更（一番最後の配列を抜き取り、先頭に追加）する処理をつけて改良して？
+// const lines = [
+//   "3 4", //路線の数を表す整数 N  駅の数を表す整数 M
+//   "0 1 2 3",
+//   "0 4 5 6",
+//   "0 7 8 9",
+//   "3", //ゴールまでに経由すべき駅の数を表す整数 X
+//   "1 4", //現在地から R_i 番目の路線の S_i 番目の駅に向かう指示
+//   "3 2",
+//   "2 2",
+// ]; //5
+// const lines = [
+//   "1 9",
+//   "0 2 7 10 13 33 74 76 82",
+//   "4",
+//   "1 6",
+//   "1 4",
+//   "1 5",
+//   "1 3",
+// ];//65
 
 ***** ***** ***** ***** ***** ***** ***** ***** */
--- js/0502.js
+++ js/0502.js
@@ -1,4 +1,95 @@
-const lines = ["3 4", "###.", "####", ".##."];
+// const lines = ["7", "WWWWW"];
+
+function checkBullet(data) {
+  const initBullets = data[1];
+  const bulletsArr = initBullets.split("");
+  const len = bulletsArr.length;
+
+  for (let i = 0; i < len; i++) {
+    const last = bulletsArr.pop();
+    bulletsArr.unshift(last);
+    let allMatch = true;
+
+    for (let j = 0; j < len; j++) {
+      if (bulletsArr[j] !== initBullets[j]) {
+        allMatch = false;
+        break;
+      }
+    }
+    if (allMatch) {
+      return i + 1;
+    }
+  }
+
+  return len;
+}
+
+console.log(checkBullet(lines));
+
+/* ***** ***** ***** ***** ***** ***** ***** ***** 
+
+const a = [ 'B', 'W', 'W', 'W', 'B', 'W', 'B' ];
+const b = [ 'B', 'W', 'W', 'W', 'B', 'W', 'B' ];
+上記の配列の中身順番も含め完全一致するか判定
+
+items = lines.slice(1);
+const square = items.map((item) => item.split(""));
+
+function checkCrossCount(square) {
+  let crossCount = 0;
+
+  for (let i = 1; i < square.length - 1; i++) {
+    const targetArray = square[i];
+    const aboveArray = square[i - 1];
+    const belowArray = square[i + 1];
+
+    for (let j = 1; j < targetArray.length - 1; j++) {
+      const target = targetArray[j];
+      const leftTarget = targetArray[j - 1];
+      const rightTarget = targetArray[j + 1];
+      const aboveTarget = aboveArray[j];
+      const belowTarget = belowArray[j];
+
+      if (target === "#" && 
+          leftTarget === "#" && 
+          rightTarget === "#" && 
+          aboveTarget === "#" && 
+          belowTarget === "#") {
+        crossCount++;
+      }
+    }
+  }
+  return crossCount;
+}
+
+console.log(checkCrossCount(square));
+
+function checkBullet(data) {
+  const initBullets = data[1];
+  const bulletsArr = initBullets.split("");
+  let shootCount = 0;
+  const len = bulletsArr.length;
+
+  for (let i = 0; i < len; i++) {
+    let allMatch = true;
+    for (let j = 0; j < len; j++) {
+      if (bulletsArr[j] !== initBullets[(i + j) % len]) {
+        allMatch = false;
+        break;
+      }
+    }
+    if (allMatch) {
+      return i + 1;
+    }
+  }
+  return len;
+}
+一回ずつ配列の中身を変更（一番最後の配列を抜き取り、先頭に追加）する処理をつけて改良して？
+
+***** ***** ***** ***** ***** ***** ***** ***** */
+
+
+// const lines = ["3 4", "###.", "####", ".##."];
 
 items = lines.slice(1);
 const square = items.map((item) => item.split(""));
