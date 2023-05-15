0877558b3dfe035e20e895527f50ab84c9285691
Buchi-18 <3690@gmail.com>
2023-05-15 11:20:20 +0900

    0514

--- js/00_main.js
+++ js/00_main.js
@@ -1,51 +1,43 @@
 const lines = [
-  "10 19",
-  ".#.##.#..###..#..##",
-  "#.#...##.##.#.....#",
-  "...#...###..#.#.#..",
-  ".##.#..##.#...#.##.",
-  ".#.#.#.##.##.#.#.#.",
-  ".###.....#..##.##..",
-  "##..#.#..#...####..",
-  "..#..#..#.#..#..##.",
-  ".###..#.##.#..###..",
-  "###...#....###.###.",
-  "6 9",
+  "5 10 5",
+  "o 5 4",
+  "f 1 5",
+  "b 1 2",
+  "k 1 5",
+  "k 2 4",
+  "1",
+  "2",
+  "1",
+  "2",
+  "2",
+  "2",
+  "2",
+  "2",
+  "1",
+  "1",
 ];
 
-const H = Number(lines[0].split(" ")[0]);
-const W = Number(lines[0].split(" ")[1]);
-const records = lines.slice(1, H + 1).map((row) => row.split(""));
-const rewrite = lines.slice(-1)[0].split(" ").map(Number);
-const targetRow = rewrite[0];
-const targetCol = rewrite[1];
-const targetRange = [
-  [0, 0],
-  [-1, 0],
-  [1, 0],
-  [0, -1],
-  [0, 1],
-]
-  .map(([row, col]) => [targetRow + row, targetCol + col])
-  .filter(([row, col]) => row >= 0 && col >= 0 && row < H && col < W);
+const [N, K, S] = lines[0].split(" ").map(Number);
+const spotData = lines
+  .slice(1, 1 + N)
+  .map((spot) => spot.split(" "))
+  .map((subArr) => [subArr[0], Number(subArr[1]), Number(subArr[2])]);
+const directions = lines.slice(1 + N).map(Number);
 
-function rewriteRecord() {
-  targetRange.forEach(([row, col]) => {
-    if (records[row][col] === ".") {
-      records[row][col] = "#";
-    } else {
-      records[row][col] = ".";
-    }
-  });
-  return records;
+const initPos = spotData[S - 1];
+
+function getSpells() {
+  const spells = [initPos[0]];
+  let currentPos = initPos;
+
+  for (let i = 0; i < K; i++) {
+    let num = currentPos[directions[i]] - 1;
+    currentPos = spotData[num];
+    spells.push(currentPos[0]);
+  }
+
+  return spells;
 }
 
-function showRecord() {
-  const recordsRow = rewriteRecord().map((row) => row.join(""));
-  return recordsRow;
-}
-
-const result = showRecord();
-for (let i = 0; i < result.length; i++) {
-  console.log(result[i]);
-}
+const result = getSpells();
+console.log(result.join(""));
